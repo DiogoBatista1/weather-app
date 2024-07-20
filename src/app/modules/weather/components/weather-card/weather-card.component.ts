b3d0a551f98260c98996fc08a8b759977dd45b0b
cypherm0nk <88748637+cypherm0nk@users.noreply.github.com>
2024-07-20 16:25:38 -0300

    create the component weather-card

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: [],
+})
+export class WeatherCardComponent {
+  @Input() weatherDatasInput!: WeatherDatas;
+  ngOnInit(): void {
+    console.log(
+      'DADOS RECEBIDOS DO COMPONENTE PAI - WEATHER-CARD',
+      this.weatherDatasInput
+    );
+  }
+}
